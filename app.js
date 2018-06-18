08945e4c5f039520110855693d40d611a961501e
Andy Aylward <aaylward@gmail.com>
2018-06-17 22:15:42 -0400

    more interesting starting point

--- app.js
+++ app.js
@@ -24,38 +24,54 @@
     }
   }
 
+  function up(row, rows) {
+    return (row + rows - 1) % rows;
+  }
+
+  function down(row, rows) {
+    return (row + rows + 1) % rows;
+  }
+  
+  function left(col, cols) {
+    return (col + cols - 1) % cols;
+  }
+
+  function right(col, cols) {
+    return (col + cols + 1) % cols;
+  }
+
   function computeState(state, row, col) {
     let numberOfLiveNeighbors = 0;
     const oldCells = state.cells;
-    const up = (row + state.rows - 1) % state.rows;
-    const down = (row + state.rows + 1) % state.rows;
-    const left = (col + state.cols - 1) % state.cols;
-    const right = (col + state.cols + 1) % state.cols;
+    const rowUp = up(row, state.rows);
+    const rowDown = down(row, state.rows);
+    const colLeft = left(col, state.cols);
+    const colRight = right(col, state.cols);
 
     const alive = oldCells[row][col];
 
-    if (oldCells[up][left]) {
+    if (oldCells[rowUp][colLeft]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[up][col]) {
+    if (oldCells[rowUp][col]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[up][right]) {
+    if (oldCells[rowUp][colRight]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[row][left]) {
+    if (oldCells[row][colLeft]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[row][right]) {
+    if (oldCells[row][colRight]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[down][left]) {
+    if (oldCells[rowDown][colLeft]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[down][col]) {
+    if (oldCells[rowDown][col]) {
       numberOfLiveNeighbors++;
     }
-    if (oldCells[down][right]) {
+    if (oldCells[rowDown][colRight]) {
       numberOfLiveNeighbors++;
     }
 
@@ -120,10 +136,75 @@
 
     state.cells = newCells;
     drawWorld(state);
-    console.log("drew world")
     if (!state.paused) {
       setTimeout(nextState, 500, state);
     }
+  }
+
+  function blinker(state, row, col) {
+    state.cells[up(row, state.rows)][col] = true;
+    state.cells[row][col] = true;
+    state.cells[down(row, state.rows)][col] = true;
+  }
+
+  function blonker(state, row, col) {
+    state.cells[row][left(col, state.cols)] = true;
+    state.cells[row][col] = true;
+    state.cells[row][right(col, state.cols)] = true;
+  }
+
+  function glider(state, row, col) {
+    blonker(state, row, col);
+    state.cells[up(row, state.rows)][col + 1] = true;
+    state.cells[up(up(row, state.rows), state.rows)][col] = true;
+  }
+
+  function probe(state, row, col) {
+    blinker(state, row, col);
+    blonker(state, row, col + 3);
+    blinker(state, row, col + 6);
+    blonker(state, row, col + 9);
+  }
+
+  function pulsar(state, row, col) {
+    blonker(state, row, col);
+    blonker(state, row + 2, col);
+
+    blonker(state, row, col + 6);
+    blonker(state, row + 2, col + 6);
+
+    blonker(state, row - 5, col + 6);
+    blonker(state, row - 5, col);
+
+    blonker(state, row + 7, col + 6);
+    blonker(state, row + 7, col);
+
+    blinker(state, row - 2, col + 2);
+    blinker(state, row - 2, col + 4);
+
+    blinker(state, row + 4, col + 2);
+    blinker(state, row + 4, col + 4);
+
+    blinker(state, row + 4, col + 9);
+    blinker(state, row + 4, col - 3);
+
+    blinker(state, row - 2, col + 9);
+    blinker(state, row - 2, col - 3);
+  }
+
+  function initializeSomeShapes(state) {
+    probe(state, 10, 10);
+    probe(state, 10, 25);
+    probe(state, 10, 50);
+
+    pulsar(state, 30, 20);
+    pulsar(state, 30, 60);
+
+    glider(state, 55, 10);
+    glider(state, 55, 15);
+    glider(state, 55, 20);
+    glider(state, 45, 10);
+    glider(state, 45, 20);
   }
 
   function start() {
@@ -139,7 +220,7 @@
         cells[row][col] = false;
       }
     }
-    
+
     const state = {
       paused: true,
       appDiv: appDiv,
@@ -150,6 +231,8 @@
       rows: 60,
       cols: 80
     };
+
+    initializeSomeShapes(state);
 
     drawWorld(state);
     attachListeners(state);
